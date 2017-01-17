bf77d9205966ee4d8d93c86943b50388506ea59d
Yifan Sun <sunyifan112358@gmail.com>
2017-01-16 21:42:43 -0500

    Adding a memory.controller interface

---
+++ os/doc.go
@@ -0,0 +1,4 @@
+// Yaotsu is an application only simulator. Therefore, it needs to emulate
+// the operating system in the simulator space. We define a Linux-like system
+// in this package.
+package os
