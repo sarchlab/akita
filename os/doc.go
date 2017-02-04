6931b2e2096581dfe82335fd4f7a832e0b5fd75c
Yifan Sun <sunyifan112358@gmail.com>
2017-02-03 22:13:47 -0500

    Remove the os and platform package from the core

--- os/doc.go
+++
@@ -1,4 +0,0 @@
-// Yaotsu is an application only simulator. Therefore, it needs to emulate
-// the operating system in the simulator space. We define a Linux-like system
-// in this package.
-package os
