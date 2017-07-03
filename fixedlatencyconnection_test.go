6d5a3748dd09f1a92ee29962faece1ae9cbc6091
Yifan Sun <sunyifan112358@gmail.com>
2017-07-03 14:30:30 -0400

    Testing fixed latency connection

--- fixedlatencyconnection_test.go
+++ fixedlatencyconnection_test.go
@@ -43,18 +43,46 @@ var _ = Describe("FixedLatencyConnection", func() {
 		Expect(func() { connection.Detach(comp1) }).To(Panic())
 	})
 
-	// It("should send with latency", func() {
-	// 	req := NewMockRequest()
-	// 	req.SetSrc(comp2)
-	// 	req.SetDst(comp1)
-	// 	req.SetSendTime(2.0)
+	It("should send with latency", func() {
+		req := NewMockRequest()
+		req.SetSrc(comp2)
+		req.SetDst(comp1)
+		req.SetSendTime(2.0)
 
-	// 	errToRet := core.NewError("something", true, 10)
-	// 	comp1.ToReceiveReq(req, errToRet)
+		connection.Send(req)
 
-	// 	err := connection.Send(req)
+		Expect(len(engine.ScheduledEvent)).To(Equal(1))
+		Expect(engine.ScheduledEvent[0].Time()).To(
+			BeNumerically("~", 2.000000002, 1e-12))
+	})
 
-	// 	Expect(err).To(BeIdenticalTo(errToRet))
-	// })
+	It("should deliever", func() {
+		req := NewMockRequest()
+		req.SetDst(comp1)
+		req.SetSendTime(2.0)
+		evt := core.NewDeliverEvent(2.0, connection, req)
+
+		comp1.ToReceiveReq(req, nil)
+
+		connection.Handle(evt)
+
+		Expect(comp1.AllReqReceived()).To(BeTrue())
+	})
+
+	It("should reschedule delievery if error", func() {
+		req := NewMockRequest()
+		req.SetDst(comp1)
+		req.SetSendTime(2.0)
+		evt := core.NewDeliverEvent(2.0, connection, req)
+
+		comp1.ToReceiveReq(req, core.NewError("", true, 2.2))
+
+		connection.Handle(evt)
+
+		Expect(comp1.AllReqReceived()).To(BeTrue())
+		Expect(len(engine.ScheduledEvent)).To(Equal(1))
+		Expect(engine.ScheduledEvent[0].Time()).To(Equal(core.VTimeInSec(2.2)))
+
+	})
 
 })
