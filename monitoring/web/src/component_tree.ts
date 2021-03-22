92f405b1328ae3e5d7714deaf36ef0bb13227db8
Ryan Gainor <rtgainor@email.wm.edu>
2021-03-22 17:17:14 +0000

    Update component_tree.ts to have a basic tree structure

--- monitoring/web/src/component_tree.ts
+++ monitoring/web/src/component_tree.ts
@@ -6,18 +6,43 @@ export function listComponents() {
         .then((res: Array<string>) => {
             res.forEach((compName: string) => {
                 console.log(compName)
-
+                let nameTree = compName.split('.')
                 let btn = document.createElement("div")
-                btn.innerHTML = compName
-                btn.addEventListener("click",
-                    () => {
-                        const detailView = new ComponentDetailView(compName)
-                        detailView.populate()
-                    })
+                //initial string
+                btn.innerHTML = nameTree[0]
+                btn.style.cursor = 'pointer'
+                btn.style.userSelect = 'none'
+                //additional
+                for(let i = 1;i<nameTree.length;i++){ 
+                    let newElement = document.createElement('div')
+                    newElement.innerHTML = nameTree[i]
+                    let indentSize = 25*i
+                    let n = indentSize.toString()
+                    newElement.style.textIndent = n + 'px'
+                    newElement.style.display = 'none'
+                    btn.appendChild(newElement)
+                }
+                //btn.addEventListener("dblclick",
+                 //   () => {
+                  //      const detailView = new ComponentDetailView(compName)
+                   //     detailView.populate()
+                    //})
+
+                function collapse(event:any){
+                    const detailView = new ComponentDetailView(compName)
+                    detailView.populate()
+                    for(let j=0;j<event.target.children.length;j++){
+                        if(event.target.children[j].style.display=='block'){
+                            event.target.children[j].style.display='none'  
+                        }else{
+                            event.target.children[j].style.display='block' 
+                        }
+                    }
+                }
+                btn.addEventListener("click", collapse)
 
                 document.getElementById('tree-container')
                     .appendChild(btn)
             })
         })
 }
-
