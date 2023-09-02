f770302038f37b2606529c915483cc96b941b37d
Yifan Sun <sunyifan112358@gmail.com>
2023-09-02 06:59:05 -0400

    Fix vite build

--- daisen/static/vite.config.js
+++ daisen/static/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
-export default defineConfig ({
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3001',
-				changeOrigin: true,
-			}
-		}
-	}
-})
+export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
+});
