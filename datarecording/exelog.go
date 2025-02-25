cf14c38f2c5f1f6447fb77cfbfa49c9a8c849201
Xuzhong Wang <xwang58@wm.edu>
2025-02-25 09:25:29 -0500

    Implement Write and Flush method for ExeLog

--- datarecording/exelog.go
+++ datarecording/exelog.go
@@ -1,32 +1,65 @@
 package datarecording
 
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
 // Struct ExeInfo is feed to SQLiteWriter
 type ExeInfo struct {
 	property string
 	value    string
 }
 
-/*
-1. Log command through init() function
-2. Convert the execution into ExeInfo object
-3. Through SQLiteWriter API we create a table, and insert all following objects
-4. Use atexit command to "automatically" flush all ExeInfom objects in a table eventually
-*/
-
 type ExeRecorder struct {
 	// ExeRecorder use SQLiteWriter to log execution
 	writer *SQLiteWriter
 
-	// Stores all the tasks as command
-	tasks ExeInfo
+	// tableName string
+	tableName string
+
+	// Stores all the entries as into a slice
+	entries []ExeInfo
 }
 
 // Write keeps track of current execution and writes it into SQLiteWriter
 func (e *ExeRecorder) Write() {
+	currentTime := time.Now()
+	startTime := currentTime.Format("2006-01-02 15:04:05")
+	timeEntry := ExeInfo{"Start Time", startTime}
+	e.entries = append(e.entries, timeEntry)
+
+	cmd := strings.Join(os.Args, " ")
+	cmdEntry := ExeInfo{"Command", cmd}
+	e.entries = append(e.entries, cmdEntry)
+
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	path := filepath.Dir(ex)
+	pathEntry := ExeInfo{"Path", path}
+	e.entries = append(e.entries, pathEntry)
+
+	sampleEntry := ExeInfo{}
+	e.tableName = "log_" + startTime
+	e.writer.CreateTable(e.tableName, sampleEntry)
 }
 
 // Flush writes data into SQLite along with program exit time
 func (e *ExeRecorder) Flush() {
+	for _, entry := range e.entries {
+		e.writer.InsertData(e.tableName, entry)
+	}
+
+	endTime := time.Now()
+	endValue := endTime.Format("2006-01-02 15:04:05")
+	timeEntry := ExeInfo{"End Time", endValue}
+	e.writer.InsertData(e.tableName, timeEntry)
+
+	e.writer.Flush()
 }
 
 // Sets SQLiteWriter
