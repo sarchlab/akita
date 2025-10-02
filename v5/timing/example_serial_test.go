91bc543297cf9ba5316338f6e163866056cc3060
Yifan Sun <profyifansun@gmail.com>
2025-10-02 13:42:09 -0400

    frequency registry example

--- v5/timing/example_serial_test.go
+++ v5/timing/example_serial_test.go
@@ -76,23 +76,3 @@ func Example_serialEngine_basic() {
 	// [mailbox] Received pong to client: pong!
 	// current time: 6 cycles
 }
-
-// Example_frequencyRegistry_twoDomains illustrates how separate domains align to the global cycle.
-func Example_frequencyRegistry_twoDomains() {
-	registry := timing.NewFrequencyRegistry()
-	cpuDomain, _ := registry.RegisterFrequency(2 * timing.GHz)
-	memDomain, _ := registry.RegisterFrequency(1 * timing.GHz)
-
-	fmt.Printf("cpu stride: %d\n", cpuDomain.Stride())
-	fmt.Printf("memory stride: %d\n", memDomain.Stride())
-	fmt.Printf("memory this tick at 3 cycles: %d\n", memDomain.ThisTick(timing.VTimeInCycle(3)))
-	fmt.Printf("memory next tick after 4 cycles: %d\n", memDomain.NextTick(timing.VTimeInCycle(4)))
-	fmt.Printf("memory two ticks later from 3 cycles: %d\n",
-		memDomain.NTicksLater(timing.VTimeInCycle(3), timing.VTimeInCycle(2)))
-	// Output:
-	// cpu stride: 1
-	// memory stride: 2
-	// memory this tick at 3 cycles: 4
-	// memory next tick after 4 cycles: 6
-	// memory two ticks later from 3 cycles: 8
-}
