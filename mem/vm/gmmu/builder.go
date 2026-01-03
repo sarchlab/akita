fbf26c0a212ef891c4abfe0b2bc024f6dd858fb5
DX990307 <xudaoxuan@ufl.edu>
2026-01-03 21:40:04 +0000

    add GMMU comp

---
+++ mem/vm/gmmu/builder.go
@@ -0,0 +1,140 @@
+package gmmu
+
+import (
+	"github.com/sarchlab/akita/v4/mem/vm"
+	"github.com/sarchlab/akita/v4/sim"
+)
+
+// A Builder can build GMMU component
+type Builder struct {
+	engine             sim.Engine
+	freq               sim.Freq
+	log2PageSize       uint64
+	pageTable          vm.PageTable
+	maxNumReqInFlight  int
+	pageWalkingLatency int
+	deviceID           uint64
+	lowModule          sim.Port
+	isRecording        bool
+	gpuIDList          []uint64
+	isPrediction       bool
+}
+
+// MakeBuilder creates a new builder
+func MakeBuilder() Builder {
+	return Builder{
+		freq:              1 * sim.GHz,
+		log2PageSize:      12,
+		maxNumReqInFlight: 16,
+		isRecording:       false,
+		isPrediction:      true,
+	}
+}
+
+func (b Builder) WithIsPrediction(isPrediction bool) Builder {
+	b.isPrediction = isPrediction
+	return b
+}
+
+// WithEngine sets the engine to be used with the GMMU
+func (b Builder) WithEngine(engine sim.Engine) Builder {
+	b.engine = engine
+	return b
+}
+
+// WithFreq sets the frequency that the GMMU to work at
+func (b Builder) WithFreq(freq sim.Freq) Builder {
+	b.freq = freq
+	return b
+}
+
+// WithLog2PageSize sets the page size that the gmmu support.
+func (b Builder) WithLog2PageSize(log2PageSize uint64) Builder {
+	b.log2PageSize = log2PageSize
+	return b
+}
+
+// WithPageTable sets the page table that the GMMU uses.
+func (b Builder) WithPageTable(pageTable vm.PageTable) Builder {
+	b.pageTable = pageTable
+	return b
+}
+
+// WithMaxNumReqInFlight sets the number of requests can be concurrently
+// processed by the GMMU.
+func (b Builder) WithMaxNumReqInFlight(maxNumReqInFlight int) Builder {
+	b.maxNumReqInFlight = maxNumReqInFlight
+	return b
+}
+
+// WithPageWalkingLatency sets the latency of page walking
+func (b Builder) WithPageWalkingLatency(pageWalkingLatency int) Builder {
+	b.pageWalkingLatency = pageWalkingLatency
+	return b
+}
+
+// WithDeviceID sets the device ID of the GMMU
+func (b Builder) WithDeviceID(deviceID uint64) Builder {
+	b.deviceID = deviceID
+	return b
+}
+
+// WithLowModule sets the low module of the GMMU
+func (b Builder) WithLowModule(p sim.Port) Builder {
+	b.lowModule = p
+	return b
+}
+
+// WithRecording sets whether the GMMU is recording
+func (b Builder) WithRecording(isRecording bool) Builder {
+	b.isRecording = isRecording
+
+	return b
+}
+
+func (b Builder) WithGPUIDList(gpuIDList []uint64) Builder {
+	b.gpuIDList = gpuIDList
+	return b
+}
+
+func (b Builder) configureInternalStates(gmmu *GMMU) {
+	gmmu.maxRequestsInFlight = b.maxNumReqInFlight
+	gmmu.latency = b.pageWalkingLatency
+	gmmu.PageAccessedByDeviceID = make(map[uint64][]uint64)
+	gmmu.deviceID = b.deviceID
+	gmmu.LowModule = b.lowModule
+	gmmu.isPrediction = b.isPrediction
+	gmmu.gpuIDList = b.gpuIDList
+	gmmu.log2PageSize = b.log2PageSize
+}
+
+func (b Builder) createPageTable(gmmu *GMMU) {
+	if b.pageTable != nil {
+		gmmu.pageTable = b.pageTable
+	} else {
+		gmmu.pageTable = vm.NewPageTable(b.log2PageSize)
+	}
+}
+
+func (b Builder) createPorts(name string, gmmu *GMMU) {
+	gmmu.topPort = sim.NewPort(gmmu, 4096, 4096, name+".TopPort")
+	gmmu.AddPort("Top", gmmu.topPort)
+	gmmu.bottomPort = sim.NewPort(gmmu, 4096, 4096, name+".BottomPort")
+	gmmu.AddPort("Bottom", gmmu.bottomPort)
+
+	gmmu.remoteMemReqs = make(map[uint64]transaction)
+}
+
+func (b Builder) Build(name string) *GMMU {
+	gmmu := new(GMMU)
+	gmmu.TickingComponent = *sim.NewTickingComponent(
+		name, b.engine, b.freq, gmmu)
+
+	// gmmu.gpuIDList = b.gpuIDList
+
+	b.createPorts(name, gmmu)
+	b.createPageTable(gmmu)
+	b.configureInternalStates(gmmu)
+
+	return gmmu
+}
---
+++ mem/vm/gmmu/comp.go
@@ -0,0 +1,295 @@
+package gmmu
+
+import (
+	"log"
+	"reflect"
+
+	"github.com/sarchlab/akita/v4/mem/vm"
+	"github.com/sarchlab/akita/v4/sim"
+	"github.com/sarchlab/akita/v4/tracing"
+)
+
+type transaction struct {
+	req       *vm.TranslationReq
+	page      vm.Page
+	cycleLeft int
+}
+
+// gmmu is the default gmmu implementation. It is also an akita Component.
+type GMMU struct {
+	sim.TickingComponent
+
+	deviceID uint64
+
+	topPort    sim.Port
+	bottomPort sim.Port
+
+	LowModule sim.Port
+
+	log2PageSize uint64
+
+	MigrationServiceProvider sim.Port
+
+	pageTable           vm.PageTable
+	latency             int
+	maxRequestsInFlight int
+
+	walkingTranslations []transaction
+	remoteMemReqs       map[uint64]transaction
+
+	toRemoveFromPTW        []int
+	PageAccessedByDeviceID map[uint64][]uint64
+
+	isRecording  bool
+	gpuIDList    []uint64
+	isPrediction bool
+}
+
+// Tick defines how the gmmu update state each cycle
+func (gmmu *GMMU) Tick() bool {
+	madeProgress := false
+
+	madeProgress = gmmu.walkPageTable() || madeProgress
+	madeProgress = gmmu.parseFromTop() || madeProgress
+	madeProgress = gmmu.fetchFromBottom() || madeProgress
+
+	return madeProgress
+}
+
+func (gmmu *GMMU) parseFromTop() bool {
+	if len(gmmu.walkingTranslations) >= gmmu.maxRequestsInFlight {
+		return false
+	}
+
+	req := gmmu.topPort.RetrieveIncoming()
+	if req == nil {
+		return false
+	}
+
+	tracing.TraceReqReceive(req, gmmu)
+
+	switch req := req.(type) {
+	case *vm.TranslationReq:
+		gmmu.startWalking(req)
+	default:
+		log.Panicf("gmmu cannot handle request of type %s", reflect.TypeOf(req))
+	}
+
+	return true
+}
+
+func (gmmu *GMMU) startWalking(req *vm.TranslationReq) {
+	translationInPipeline := transaction{
+		req:       req,
+		cycleLeft: gmmu.latency,
+	}
+
+	gmmu.walkingTranslations = append(gmmu.walkingTranslations, translationInPipeline)
+}
+
+func (gmmu *GMMU) walkPageTable() bool {
+	madeProgress := false
+	for i := 0; i < len(gmmu.walkingTranslations); i++ {
+		if gmmu.walkingTranslations[i].cycleLeft > 0 {
+			gmmu.walkingTranslations[i].cycleLeft--
+			madeProgress = true
+			continue
+		}
+		req := gmmu.walkingTranslations[i].req
+
+		page, _ := gmmu.pageTable.Find(req.PID, req.VAddr)
+
+		if page.DeviceID == gmmu.deviceID {
+			madeProgress = gmmu.finalizePageWalk(i) || madeProgress
+		} else {
+			madeProgress = gmmu.processRemoteMemReq(i) || madeProgress
+		}
+	}
+
+	gmmu.removeCompletedTranslations()
+
+	return madeProgress
+}
+
+func (gmmu *GMMU) removeCompletedTranslations() {
+	if len(gmmu.toRemoveFromPTW) == 0 {
+		return
+	}
+
+	toRemoveSet := make(map[int]bool, len(gmmu.toRemoveFromPTW))
+	for _, idx := range gmmu.toRemoveFromPTW {
+		toRemoveSet[idx] = true
+	}
+
+	tmp := gmmu.walkingTranslations[:0]
+	for i := 0; i < len(gmmu.walkingTranslations); i++ {
+		if !toRemoveSet[i] {
+			tmp = append(tmp, gmmu.walkingTranslations[i])
+		}
+	}
+	gmmu.walkingTranslations = tmp
+	gmmu.toRemoveFromPTW = nil
+}
+
+func (gmmu *GMMU) processRemoteMemReq(walkingIndex int) bool {
+	walking := gmmu.walkingTranslations[walkingIndex].req
+
+	gmmu.remoteMemReqs[walking.VAddr] = gmmu.walkingTranslations[walkingIndex]
+
+	req := vm.TranslationReqBuilder{}.
+		WithSrc(gmmu.bottomPort.AsRemote()).
+		WithDst(gmmu.LowModule.AsRemote()).
+		WithPID(walking.PID).
+		WithVAddr(walking.VAddr).
+		WithDeviceID(walking.DeviceID).
+		Build()
+
+	err := gmmu.bottomPort.Send(req)
+
+	if err != nil {
+		return false
+	}
+
+	gmmu.toRemoveFromPTW = append(gmmu.toRemoveFromPTW, walkingIndex)
+
+	return true
+}
+
+func (gmmu *GMMU) finalizePageWalk(
+	walkingIndex int,
+) bool {
+	req := gmmu.walkingTranslations[walkingIndex].req
+	page, _ := gmmu.pageTable.Find(req.PID, req.VAddr)
+
+	gmmu.walkingTranslations[walkingIndex].page = page
+
+	return gmmu.doPageWalkHit(walkingIndex)
+}
+
+func (gmmu *GMMU) pageNeedMigrate(walking transaction) bool {
+	if walking.req.DeviceID == walking.page.DeviceID {
+		return false
+	}
+
+	if !walking.page.Unified {
+		return false
+	}
+
+	if walking.page.IsPinned {
+		return false
+	}
+
+	return true
+}
+
+func (gmmu *GMMU) doPageWalkHit(
+	walkingIndex int,
+) bool {
+	if !gmmu.topPort.CanSend() {
+		return false
+	}
+	walking := gmmu.walkingTranslations[walkingIndex]
+
+	rsp := vm.TranslationRspBuilder{}.
+		WithSrc(gmmu.topPort.AsRemote()).
+		WithDst(walking.req.Src).
+		WithRspTo(walking.req.ID).
+		WithPage(walking.page).
+		Build()
+
+	gmmu.topPort.Send(rsp)
+
+	gmmu.toRemoveFromPTW = append(gmmu.toRemoveFromPTW, walkingIndex)
+
+	if !gmmu.sendToGMMU(walking) {
+		return false
+	}
+
+	tracing.TraceReqComplete(walking.req, gmmu)
+
+	return true
+}
+
+func (gmmu *GMMU) toRemove(index int) bool {
+	for _, removeIndex := range gmmu.toRemoveFromPTW {
+		if removeIndex == index {
+			return true
+		}
+	}
+	return false
+}
+
+func (gmmu *GMMU) fetchFromBottom() bool {
+	if !gmmu.topPort.CanSend() {
+		return false
+	}
+
+	rsp := gmmu.bottomPort.RetrieveIncoming()
+	if rsp == nil {
+		return false
+	}
+
+	tracing.TraceReqReceive(rsp, gmmu)
+
+	switch rsp := rsp.(type) {
+	case *vm.TranslationRsp:
+		return gmmu.handleTranslationRsp(rsp)
+	default:
+		log.Panicf("gmmu cannot handle request of type %s", reflect.TypeOf(rsp))
+	}
+
+	return true
+}
+
+func (gmmu *GMMU) handleTranslationRsp(response *vm.TranslationRsp) bool {
+	reqTransaction := gmmu.remoteMemReqs[uint64(response.Page.VAddr)]
+
+	if reqTransaction.req == nil {
+		log.Panicf("Cannot find matching request for response %+v", response)
+	}
+
+	if !gmmu.topPort.CanSend() {
+		return false
+	}
+
+	rsp := vm.TranslationRspBuilder{}.
+		WithSrc(gmmu.topPort.AsRemote()).
+		WithDst(reqTransaction.req.Src).
+		WithRspTo(response.ID).
+		WithPage(response.Page).
+		Build()
+
+	gmmu.topPort.Send(rsp)
+
+	delete(gmmu.remoteMemReqs, uint64(response.Page.VAddr))
+	return true
+}
+
+func (gmmu *GMMU) GetDeviceID() uint64 {
+	return gmmu.deviceID
+}
+
+func (gmmu *GMMU) sendToGMMU(walking transaction) bool {
+	if !gmmu.topPort.CanSend() {
+		return false
+	}
+
+	vAddr := walking.req.VAddr
+	page, found := gmmu.pageTable.Find(walking.req.PID, vAddr)
+
+	if !found {
+		log.Panicf("Page not found in GMMU page table. PID: %d, VAddr: %#x",
+			walking.req.PID, vAddr)
+	}
+
+	req := walking.req
+	rsp := vm.TranslationRspBuilder{}.
+		WithSrc(gmmu.topPort.AsRemote()).
+		WithDst(req.Src).
+		WithPage(page).
+		Build()
+
+	gmmu.topPort.Send(rsp)
+
+	return true
+}
