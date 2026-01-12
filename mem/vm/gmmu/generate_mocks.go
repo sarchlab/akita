99c4e0b98df1a20fe443bd2ce4b916485b52d0ee
DX990307 <xudaoxuan@ufl.edu>
2026-01-12 19:13:00 +0000

    fux bug

--- mem/vm/gmmu/generate_mocks.go
+++ mem/vm/gmmu/generate_mocks.go
@@ -1,5 +1,6 @@
-//go:generate mockgen -destination=mock_sim_test.go -package=gmmu_test -write_package_comment=false github.com/sarchlab/akita/v4/sim Engine,Port
-//go:generate mockgen -destination=mock_vm_test.go -package=gmmu_test -write_package_comment=false github.com/sarchlab/akita/v4/mem/vm PageTable
-//go:generate mockgen -destination=mock_mem_test.go -package=gmmu_test -write_package_comment=false github.com/sarchlab/akita/v4/mem/mem AddressToPortMapper
+//go:generate mockgen -destination=mock_engine.go -package=gmmu github.com/sarchlab/akita/v4/sim Engine
+//go:generate mockgen -destination=mock_port.go -package=gmmu github.com/sarchlab/akita/v4/sim Port
+//go:generate mockgen -destination=mock_vm.go -package=gmmu github.com/sarchlab/akita/v4/mem/vm PageTable
+//go:generate mockgen -destination=mock_mem.go -package=gmmu github.com/sarchlab/akita/v4/mem/mem AddressToPortMapper
 
 package gmmu
