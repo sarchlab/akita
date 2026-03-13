70abdfc73f9f278662564e08fa792b8378785749
Athena <athena@tbc.ai>
2026-03-12 23:15:28 -0400

    [Sage] Rename test suite to SimpleCache Suite

--- v5/mem/cache/simplecache/simplecache_suite_test.go
+++ v5/mem/cache/simplecache/simplecache_suite_test.go
@@ -10,10 +10,10 @@ import (
 )
 
 //go:generate mockgen -destination "mock_sim_test.go" -package $GOPACKAGE -write_package_comment=false github.com/sarchlab/akita/v5/sim Port
-func TestWriteAround(t *testing.T) {
+func TestSimpleCache(t *testing.T) {
 	log.SetOutput(GinkgoWriter)
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Write-Around Suite")
+	RunSpecs(t, "SimpleCache Suite")
 }
 
 func TestValidateState(t *testing.T) {
