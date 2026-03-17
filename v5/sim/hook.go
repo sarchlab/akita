58b8b4d39a282c7b481fb4c50413a3ad8847fdf3
Yifan Sun <profyifansun@gmail.com>
2026-03-17 18:58:51 -0400

    [Hugo] Create v5/hooking package and add sim compatibility aliases (#106)
    - Create v5/hooking/hook.go with HookPos, HookCtx, Hook, Hookable,
      HookableBase, and NewHookableBase extracted from v5/sim/hook.go
    - Create v5/hooking/doc.go with package documentation
    - Update v5/sim/hook.go to use type aliases and var shim pointing to
      the new hooking package
    - Keep HookPosBeforeEvent and HookPosAfterEvent in sim
    - v5/hooking has no imports from sim or tracing (leaf package)
    - go build ./... passes with no errors
    
    Co-authored-by: Athena <athena@tbc.ai>

--- v5/sim/hook.go
+++ v5/sim/hook.go
@@ -1,84 +1,29 @@
 package sim
 
+import "github.com/sarchlab/akita/v5/hooking"
+
 // HookPos defines the enum of possible hooking positions.
-type HookPos struct {
-	Name string
-}
+type HookPos = hooking.HookPos
 
 // HookCtx is the context that holds all the information about the site that a
 // hook is triggered.
-type HookCtx struct {
-	Domain Hookable
-	Pos    *HookPos
-	Item   interface{}
-	Detail interface{}
-}
+type HookCtx = hooking.HookCtx
 
 // Hookable defines an object that accept Hooks.
-type Hookable interface {
-	// AcceptHook registers a hook.
-	AcceptHook(hook Hook)
+type Hookable = hooking.Hookable
 
-	// NumHooks returns the number of hooks registered.
-	NumHooks() int
+// Hook is a short piece of program that can be invoked by a hookable object.
+type Hook = hooking.Hook
 
-	// Hooks returns all the hooks registered.
-	Hooks() []Hook
-}
+// A HookableBase provides some utility function for other type that implement
+// the Hookable interface.
+type HookableBase = hooking.HookableBase
+
+// NewHookableBase creates a HookableBase object.
+var NewHookableBase = hooking.NewHookableBase
 
 // HookPosBeforeEvent is a hook position that triggers before handling an event.
 var HookPosBeforeEvent = &HookPos{Name: "BeforeEvent"}
 
 // HookPosAfterEvent is a hook position that triggers after handling an event.
 var HookPosAfterEvent = &HookPos{Name: "AfterEvent"}
-
-// Hook is a short piece of program that can be invoked by a hookable object.
-type Hook interface {
-	// Func determines what to do if hook is invoked.
-	Func(ctx HookCtx)
-}
-
-// A HookableBase provides some utility function for other type that implement
-// the Hookable interface.
-type HookableBase struct {
-	hookList []Hook
-}
-
-// NewHookableBase creates a HookableBase object.
-func NewHookableBase() *HookableBase {
-	h := new(HookableBase)
-	h.hookList = make([]Hook, 0)
-
-	return h
-}
-
-// NumHooks returns the number of hooks registered.
-func (h *HookableBase) NumHooks() int {
-	return len(h.hookList)
-}
-
-// Hooks returns all the hooks registered.
-func (h *HookableBase) Hooks() []Hook {
-	return h.hookList
-}
-
-// AcceptHook register a hook.
-func (h *HookableBase) AcceptHook(hook Hook) {
-	h.mustNotHaveDuplicatedHook(hook)
-	h.hookList = append(h.hookList, hook)
-}
-
-func (h *HookableBase) mustNotHaveDuplicatedHook(hook Hook) {
-	for _, h := range h.hookList {
-		if h == hook {
-			panic("duplicated hook")
-		}
-	}
-}
-
-// InvokeHook triggers the register Hooks.
-func (h *HookableBase) InvokeHook(ctx HookCtx) {
-	for _, hook := range h.hookList {
-		hook.Func(ctx)
-	}
-}
